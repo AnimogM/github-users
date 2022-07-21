3ebf877a49a4dbd26837ec6986b45a02bd703fdc
Gomina Maryam <gominamaryam@gmail.com>
2022-07-21 17:33:23 +0100

    updated pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
+import { Box } from '@chakra-ui/react';
+import {Route, Routes,} from "react-router"
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 function App() {
   return (
-    
+    <Routes>
+      <Route path="/" exact />
+      <Route path="/login" element={<Login />} />
+      <Route path='signup' element={<Signup/>}/>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowerRouter } from "react-router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowerRouter>
+        <App />
+      </BrowerRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
